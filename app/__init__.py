53e23420ab6226bb6a4782eb902aef4fd01dd975
Meloni <meloni.langat@student.moringaschool.com>
2022-03-08 22:23:36 +0300

    create app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
   app = Flask(__name__)
-  #create app config
+  #create app configurations
   app.config.from_object(config_options[config_name])
   
   bootstrap.init_app(app)
