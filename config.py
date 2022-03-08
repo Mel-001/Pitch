260a1d3f5d9aff15c58c6896dd7ae5788b01f340
Meloni <meloni.langat@student.moringaschool.com>
2022-03-08 22:08:22 +0300

    create config file

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = 'melonie'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://melonie:Access@localhost/pitchapp'
+
+   
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+        
+    '''
+    
+   
+   
+    
+    
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class 
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://melonie:Access@localhost/pitchapp'
+  
+
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
