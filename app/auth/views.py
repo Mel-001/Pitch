6e4c3b560a5b37be2bac2471f62a7bbf910a9b8c
Meloni <meloni.langat@student.moringaschool.com>
2022-03-09 17:14:22 +0300

    modify files

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, flash
 from . import auth
+from .. import db
 from flask_login import login_user, logout_user, login_required
 from ..models import User
 from .forms import LoginForm, RegistrationForm
